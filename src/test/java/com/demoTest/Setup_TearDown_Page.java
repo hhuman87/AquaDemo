06bec1ce268dc008aa777de9bb61e1cab122c80c
Herman Human <herman.human@infocarehealth.com>
2024-11-27 21:02:06 +0200

    Update project

--- src/test/java/com/demoTest/Setup_TearDown_Page.java
+++ src/test/java/com/demoTest/Setup_TearDown_Page.java
@@ -2,6 +2,7 @@ package com.demoTest;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoTest.helpers.TestSetup;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -19,21 +20,22 @@ public class Setup_TearDown_Page {
 
     @BeforeAll
     public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
 //        Configuration.browserSize = "1280x800";
 //        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com");
+//        driver = new ChromeDriver();
+//        driver.get("https://www.saucedemo.com");
 //        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
 //        open("https://www.saucedemo.com");
+        TestSetup.setupSelenide();
     }
 
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-    }
+//    @AfterEach
+//    public void tearDown() {
+//        driver.quit();
+//    }
 }
