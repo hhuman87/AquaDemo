7734e8ab53a38cf05888cc4a491f0a165de4b37f
Herman Human <herman.human@infocarehealth.com>
2024-11-27 18:54:07 +0200

    Update project

--- src/test/java/com/demoTest/Setup_TearDown_Page.java
+++ src/test/java/com/demoTest/Setup_TearDown_Page.java
@@ -2,24 +2,38 @@ package com.demoTest;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class Setup_TearDown_Page {
 
+    WebDriver driver;
+
     @BeforeAll
     public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebDriverManager.chromedriver().setup();
+//        Configuration.browserSize = "1280x800";
+//        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @BeforeEach
     public void setUp() {
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("https://www.saucedemo.com");
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+//        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+//        open("https://www.saucedemo.com");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
 }
