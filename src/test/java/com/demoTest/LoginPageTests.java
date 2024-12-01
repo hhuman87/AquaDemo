e53c372b3a13684a84eef12b821cbb88e4846e9c
human <humanherman@yahoo.com>
2024-12-01 15:54:34 +0200

    New Project commit

--- src/test/java/com/demoTest/LoginPageTests.java
+++ src/test/java/com/demoTest/LoginPageTests.java
@@ -1,6 +1,5 @@
 package com.demoTest;
 
-import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,6 @@ public class LoginPageTests extends Setup_TearDown_Page {
     private final LoginPageSelectors loginPageSelectors = new LoginPageSelectors();
 
     @Test
-    @Feature("SauceDemo login")
     @DisplayName("Login to SauceDemo")
     public void login_To_SauceDemo() {
         loginPageSelectors.input_Username("standard_user");
