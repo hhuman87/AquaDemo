d7dac971c4ba4ed67ec7951122671cec951b017e
human <humanherman@yahoo.com>
2024-12-01 22:21:54 +0200

    New Project update

---
+++ src/test/java/apiTests/reqres/GetTest.java
@@ -0,0 +1,23 @@
+package apiTests.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetTest {
+
+    @Test
+    public void getTest() {
+
+        baseURI = "https://reqres.in/api";
+
+        given().
+                get("/users?page=2").
+        then().
+                statusCode(200).
+                body("data[4].first_name",equalTo("George")).
+                body("data.first_name", hasItems("George", "Rachel")).
+                log().all();
+    }
+}
