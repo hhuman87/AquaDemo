9f7fed8b018a5be4ba74ad5590b80f0e95c0df34
human <humanherman@yahoo.com>
2024-12-02 08:43:22 +0200

    New Project update

--- src/test/java/apiTests/reqres/GetTest.java
+++ src/test/java/apiTests/reqres/GetTest.java
@@ -1,21 +1,40 @@
 package apiTests.reqres;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class GetTest {
 
+    private Integer userID;
+
     @Test
     public void getTest() {
         baseURI = "https://reqres.in/api";
 
-        given().
-                get("/users?page=2").
-        then().
-                statusCode(200).
-                body("data[4].first_name",equalTo("George")).
-                body("data.first_name", hasItems("George", "Rachel")).
-                log().all();
+        getUserID();
+        validateUserID();
+    }
+
+    public void getUserID() {
+        Response response = given()
+                .get("/users?page=2").
+                then()
+                .statusCode(200)
+                .body("page",equalTo(2))
+                .body("data[4].first_name",equalTo("George"))
+                .body("data.first_name", hasItems("George", "Rachel"))
+                .extract().response();
+
+        userID = response.path("data[1].id");
+        System.out.println("User ID is :"+userID);
+    }
+
+    public void validateUserID() {
+        given()
+                .get("/users?page=2").
+                then()
+                .body("data[1].id",equalTo(userID));
     }
 }
