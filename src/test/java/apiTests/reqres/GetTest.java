f77a68b5e8d182839f4438d8eda55114b2902c48
human <humanherman@yahoo.com>
2024-12-02 11:55:21 +0200

    New Project update

--- src/test/java/apiTests/reqres/GetTest.java
+++ src/test/java/apiTests/reqres/GetTest.java
@@ -10,21 +10,22 @@ public class GetTest {
     private Integer userID;
 
     @Test
-    public void getTest() {
+    public void getUserIDTest() {
         baseURI = "https://reqres.in/api";
         getUserID();
         validateUserID();
     }
 
     public void getUserID() {
-        Response response = given()
+        Response response =
+            given()
                 .get("/users?page=2").
-                then()
+            then()
                 .statusCode(200)
                 .body("page",equalTo(2))
                 .body("data[4].first_name",equalTo("George"))
                 .body("data.first_name", hasItems("George", "Rachel"))
-                .extract().response();
+            .extract().response();
 
         userID = response.path("data[1].id");
         System.out.println("User ID is :"+userID);
@@ -33,7 +34,7 @@ public class GetTest {
     public void validateUserID() {
         given()
                 .get("/users?page=2").
-                then()
+        then()
                 .body("data[1].id",equalTo(userID));
     }
 }
---
+++ src/test/java/apiTests/reqres/PostTest.java
@@ -0,0 +1,57 @@
+package apiTests.reqres;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class PostTest {
+
+    private String token;
+    private String contentType = "application/json; charset=utf-8";
+
+    @Test
+    public void postUser() {
+        baseURI = "https://reqres.in/api";
+        getUserToken();
+        validateLoginSuccessful();
+    }
+
+    public void getUserToken() {
+        Response response =
+            given()
+                .header("Content-Type", contentType)
+                .body("{" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"pistol\"\n" +
+                        "}")
+            .when()
+                .post("/register")
+            .then()
+                .statusCode(200)
+            .extract().response();
+
+        token = response.path("token");
+        System.out.println("Token is :"+token);
+    }
+
+    public void validateLoginSuccessful() {
+        Response response =
+            given()
+                .header("Content-Type", contentType)
+                .body("{" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+            .when()
+                .post("/login")
+            .then()
+                .statusCode(200)
+            .extract().response();
+
+        String expectedToken = response.path("token");
+        System.out.println("Expected token is :"+expectedToken);
+        Assertions.assertEquals(expectedToken, token);
+    }
+}
