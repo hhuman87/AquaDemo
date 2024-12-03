b4f2a61cfcdb1d727c971b5b7d8c456da2932f9e
human <humanherman@yahoo.com>
2024-12-03 12:04:01 +0200

    New Project update

--- src/test/java/dbTests/ReadExcel.java
+++ src/test/java/dbTests/ReadExcel.java
@@ -1,4 +1,19 @@
 package dbTests;
 
+import forAllTests.utils.ExcelUtils;
+import org.junit.Test;
+
 public class ReadExcel {
+
+    @Test
+    public void readDataFromExcel() {
+        String excelPath = "./ExcelTestData/Test1Data.xlsx";
+        String sheetName = "Users";
+
+        ExcelUtils excel = new ExcelUtils(excelPath,sheetName);
+
+        excel.getRowCount();
+        excel.getCellValue(1,0);
+    }
+
 }
--- src/test/java/forAllTests/utils/ExcelUtils.java
+++ src/test/java/forAllTests/utils/ExcelUtils.java
@@ -2,34 +2,38 @@ package forAllTests.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.Test;
 
-import java.io.IOException;
 import java.util.Arrays;
 
 public class ExcelUtils {
 
     private static final Logger log = LogManager.getLogger(ExcelUtils.class);
 
-    @Test
-    public void getRowCount() {
+    static XSSFWorkbook workbook;
+    static XSSFSheet sheet;
 
+    public ExcelUtils(String excelPath, String sheetName) {
         try {
-            String excelPath = "./ExcelTestData/Test1Data.xlsx";
-            XSSFWorkbook workbook = new XSSFWorkbook(excelPath);
-            XSSFSheet sheet = workbook.getSheet("Users");
-
-            int rowCount = sheet.getPhysicalNumberOfRows();
-            System.out.println("No of rows: " + rowCount);
-
-
-
+            workbook = new XSSFWorkbook(excelPath);
+            sheet = workbook.getSheet("Users");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             log.error(String.valueOf(e.getCause()));
             System.out.println(Arrays.toString(e.getStackTrace()));
         }
+    }
+
+    public void getRowCount() {
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        System.out.println("No of rows: " + rowCount);
+    }
+
+    public void getCellValue(int rowNum, int colNum) {
+        DataFormatter formatter = new DataFormatter();
+        Object cellValue = formatter.formatCellValue(sheet.getRow(rowNum).getCell(colNum));
+        System.out.println("The cell value is: " + cellValue);
     }
 }
